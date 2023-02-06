d15ad579cafa688297a16497a5365fc21321f136
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2023-02-06 03:46:21 +0200

    setting up getting all tasks & creating a task & get a single task & delete a task, and handle the error (with try and catch)

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,24 +1,61 @@
 const Task = require("../models/Task")
 
-const getAllTasks = (req, res) => {
-  res.send("get all tasks")
+// getting all tasks
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
+// creating a new task
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body)
-  res.status(201).json({ task })
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id })
+// getting a single task
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params
+    const task = await Task.findOne({ _id: taskId })
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskId}` })
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const updateTask = (req, res) => {
   res.send("update task")
 }
 
-const deleteTask = (req, res) => {
-  res.send("delete task")
+// deleting a task
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params
+    const task = await Task.findByIdAndDelete({ _id: taskId })
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskId}` })
+    }
+
+    res.status(200).json({ task })
+    // res.status(200).send()
+    // res.status(200).json({ task: null, status: "success" })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 module.exports = {
